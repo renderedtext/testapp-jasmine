dee2a143c485ce697161f14d8d40dc279657b6b2
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-11-18 15:00:15 +0100

    Generate Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestappJasmine::Application.config.secret_token = '9d9fec5b826fbc5fde1689fdd8e011680c191b9b57c5492bf2d6d359a0e65887c90250895c0f9945d800ff10d2befdf27db458e39581cb62b43883f6b59c4ed6'
